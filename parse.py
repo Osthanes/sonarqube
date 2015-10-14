a4d59a69da53d929778b8cbf4baff425c3f9dbbe
Evan Lobeto <elobeto@us.ibm.com>
2015-10-14 14:55:45 -0400

    debugging

--- parse.py
+++ parse.py
@@ -4,7 +4,10 @@ import sys
 import json
 import os
 
+ipJson = ""
+
 with open('ipJSON.json') as jsonFile:
+    global ipJson
     ipJson = json.load(jsonFile)
 
 print ipJson[0]["NetworkSettings"]["PublicIpAddress"]
